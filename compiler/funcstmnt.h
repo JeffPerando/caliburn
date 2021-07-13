b903b2963c744a512729f0ad9d66ef91f6f6d31e
Jeff P <realmsofcaelum@gmail.com>
2021-07-13 04:30:44 -0700

    Updated FunctionStatement
    Still not ready, but it's getting there

--- compiler/funcstmnt.h
+++ compiler/funcstmnt.h
@@ -6,31 +6,61 @@
 
 namespace caliburn
 {
-	struct FuncArg
+	enum class FuncControl : uint32_t
 	{
-		NamedSSA type, name;
-		ValueStatement* argDefault = nullptr;
+		None = 0x00,
+		Inline = 0x01,
+		DontLine = 0x02,
+		Pure = 0x04,
+		Const = 0x08
+
 	};
 
-	struct FuncStatement : public Statement
+	struct FuncArg
 	{
-		NamedSSA returnType;
+		std::string type, name;
+		ValueStatement* argDefault = nullptr;
+		uint32_t ssa = 0;
+
+	};
+
+	struct FunctionStatement : public Statement
+	{
+		std::string returnType = "";
 		std::string name = "FORGOT TO NAME YOUR FUNCTION!";
 		uint32_t ssa = 0;
 		std::vector<FuncArg> args;
+		FuncControl control = FuncControl::Inline;
 		uint32_t calls = 0;
 		Statement* funcBody = nullptr;
 
-		FuncStatement() : Statement(0) {}
+		FunctionStatement() : Statement(0) {}
 
 		uint32_t toSPIRV(SpirVAssembler* codeAsm)
 		{
 			ssa = codeAsm->newAssign();
 
+			uint32_t returnTypeSSA = codeAsm->pushType(returnType);
+
 			codeAsm->push(spirv::OpFunction());
-			//TODO code
+			codeAsm->push(returnTypeSSA); //TODO proper type resolving
+			codeAsm->push(ssa);
+			codeAsm->push((uint32_t)control);
+			codeAsm->push(returnTypeSSA);
+
+			for (auto arg : args)
+			{
+				arg.ssa = codeAsm->newAssign();
+				codeAsm->pushAll({ spirv::OpFunctionParameter(), codeAsm->pushType(arg.type), arg.ssa });
+
+			}
+
+			funcBody->toSPIRV(codeAsm);
+
 			codeAsm->push(spirv::OpFunctionEnd());
+
 		}
+
 	};
 
 }
