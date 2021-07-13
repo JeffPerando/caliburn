7d408c613a9176835a4029f21e13712f8af50829
Jeff P <realmsofcaelum@gmail.com>
2021-07-13 04:31:16 -0700

    Cleaned up ScopeStatement methods

--- compiler/scopestmnt.h
+++ compiler/scopestmnt.h
@@ -1,8 +1,6 @@
 
 #pragma once
 
-#include "ctrlstmnt.h"
-#include "fieldstmnt.h"
 #include "statement.h"
 
 namespace caliburn
@@ -10,31 +8,10 @@ namespace caliburn
 	struct ScopeStatement : public ContextStmnt
 	{
 		std::vector<Statement*> innerCode;
-		std::vector<FieldStatement*> fields;
 		ScopeStatement() : ContextStmnt(0) {}
 		~ScopeStatement() { Statement::~Statement(); }
 
 		uint32_t toSPIRV(SpirVAssembler* codeAsm)
-		{
-			return 0;
-		}
-
-		uint32_t addField(std::string name, Visibility vis)
-		{
-			return 0;
-		}
-
-		uint32_t resolveType(std::string id)
-		{
-			return 0;
-		}
-
-		uint32_t resolveName(std::string id)
-		{
-			return 0;
-		}
-
-		uint32_t resolveIdentifier(std::string id)
 		{
 			return 0;
 		}
