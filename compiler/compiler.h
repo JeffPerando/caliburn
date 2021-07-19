051ed543b12325d97a264148f8ce04df4070d3fa
Jeff P <realmsofcaelum@gmail.com>
2021-07-19 09:59:06 -0700

    Started work on game compiler interface.

---
+++ compiler/compiler.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "parser.h"
+
+namespace caliburn
+{
+	class Compiler
+	{
+		int32_t compile(std::string file, std::vector<std::string>& compiled)
+		{
+			return -1;
+		}
+
+	};
+
+}
