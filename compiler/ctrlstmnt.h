1560a60309f9b234b585c11a2ee6d57867c4a2a9
Jeff P <realmsofcaelum@gmail.com>
2021-07-19 08:42:45 -0700

    Removed pointer tirade, added while statements

--- compiler/ctrlstmnt.h
+++ compiler/ctrlstmnt.h
@@ -21,15 +21,6 @@ namespace caliburn
 			uint32_t endSSA = codeAsm->newAssign();
 			uint32_t elseSSA = endSSA;
 
-			//this assumes the else branch pointer is either null or valid
-			//which imo is a safe assumption, but lord knows there's certain
-			//people who think null is evil.
-			//not gonna tirade about pointers, but let me ask you this:
-			//if I have a pointer that's some invalid, magic number, how am
-			//I going to validate it without knowing the magic number? And how
-			//would others know to use THAT magic number? how will we prevent
-			//other compilers from using other, equally arbitrary magic num-- ah
-			//ffs this is a tirade, nvm... please just use null or a valid pointer
 			if (elseBranch)
 			{
 				elseSSA = codeAsm->newAssign();
@@ -117,6 +108,58 @@ namespace caliburn
 				spirv::OpLabel(), mergeSSA
 				});
 			
+		}
+
+	};
+
+	struct WhileStatement : public Statement
+	{
+		ValueStatement* cond = nullptr;
+		Statement* loop = nullptr;
+
+		WhileStatement() : Statement(0) {}
+
+		uint32_t toSPIRV(SpirVAssembler* codeAsm)
+		{
+			uint32_t mergeSSA = codeAsm->newAssign();
+			uint32_t contSSA = codeAsm->newAssign();
+			uint32_t startSSA = codeAsm->newAssign();
+			uint32_t bodySSA = codeAsm->newAssign();
+			uint32_t loopSSA = codeAsm->newAssign();
+
+			//while
+			codeAsm->pushAll({
+				spirv::OpBranch(), startSSA,
+				spirv::OpLabel(), startSSA,
+				//for
+				spirv::OpLoopMerge(0), mergeSSA, contSSA, 0, //TODO base flag off optimize level
+				//workaround for OpLoopMerge needing a branch op after
+				spirv::OpBranch(), bodySSA,
+				spirv::OpLabel(), bodySSA });
+
+			// true
+			uint32_t condSSA = cond->toSPIRV(codeAsm);
+
+			//branch
+			codeAsm->pushAll({
+				spirv::OpBranchConditional(0), condSSA, loopSSA, mergeSSA,
+				//{
+				spirv::OpLabel(), loopSSA });
+
+			loop->toSPIRV(codeAsm);
+
+			codeAsm->pushAll({
+				spirv::OpBranch(), contSSA,
+				spirv::OpLabel(), contSSA });
+			//}
+			
+			codeAsm->pushAll({
+				//loop back
+				spirv::OpBranch(), startSSA,
+				//merge point
+				spirv::OpLabel(), mergeSSA
+				});
+
 		}
 
 	};
