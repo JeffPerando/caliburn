d5f80c615e3edd32dd2b06fe98dbfcb9198b3a3a
Jeff P <realmsofcaelum@gmail.com>
2021-07-21 03:17:33 -0700

    Added more statements: Import, Using, and Typedef.
    Import: import module; very important
    Using: using namespace; useful for keeping code concise.
    Typedef: type x = y; basically equivalent to C++11 using x = y; statement.

---
+++ compiler/miscstmt.h
@@ -0,0 +1,44 @@
+
+#pragma once
+
+#include "statement.h"
+
+namespace caliburn
+{
+	struct ImportStatement : public Statement
+	{
+		std::string const importedModule;
+
+		ImportStatement(std::string m) : importedModule(m), Statement(0) {}
+		~ImportStatement() {}
+
+		uint32_t toSPIRV(SpirVAssembler* codeAsm)
+		{
+			//No complaining
+			return 0;
+		}
+
+	};
+
+	struct UsingStatement : public Statement
+	{
+		std::string const usedNamespace;
+
+		UsingStatement(std::string n) : usedNamespace(n), Statement(0) {}
+		~UsingStatement() {}
+
+		uint32_t toSPIRV(SpirVAssembler* codeAsm)
+		{
+			return 0;
+		}
+
+	};
+
+	struct TypedefStatement : public Statement
+	{
+		std::string name = "";
+		ParsedType* actualType = nullptr;
+
+	};
+
+}
