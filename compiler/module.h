2759c069b252ebfa03a2eb501da81ce0bbb3e765
Jeff P <realmsofcaelum@gmail.com>
2021-07-21 02:55:20 -0700

    Expanded on Module, see comment below.
    I've decided that a "pre-compiled" module will just contain that module's AST. Probably not the best way to go about it, but the alternative is taking precompiled SPIR-V and renaming the SSAs. Not fun nor efficient. Also not flexible or compatible with LLVM. Finally, could write a Caliburn Intermediary Representation... but, no. Just, no.

--- compiler/module.h
+++ compiler/module.h
@@ -1,8 +1,31 @@
 
 #pragma once
 
+#include <map>
+#include <string>
+
+#include "statement.h"
+
 namespace caliburn
 {
+	//TODO Eventually make a virtual with both precompiled and parsed code variants
+	//It'll mean potentially doubling up on Statement supers, but ehh...
+	class Module
+	{
+		std::map<std::string, Statement*> declarations;
 	
+	public:
+		void putDecl(std::string name, Statement* stmt)
+		{
+			declarations.emplace(name, stmt);
+
+		}
+
+		Statement* resolve(std::string name)
+		{
+			return declarations[name];
+		}
+
+	};
 
 }
