d58bba2620349f9e6060af8b6a0f743a9c50eba4
Jeff P <realmsofcaelum@gmail.com>
2021-10-01 20:39:40 -0700

    Moved CaliburnAssembler implementations to the cpp file; used constants

--- compiler/assembler.cpp
+++ compiler/assembler.cpp
@@ -3,6 +3,97 @@
 
 using namespace caliburn;
 
+CaliburnAssembler::CaliburnAssembler()
+{
+	//int types
+	for (int bits = MIN_INT_BITS_SUPPORTED;
+		bits <= MAX_INT_BITS_SUPPORTED; bits *= 2)
+	{
+		auto intType = new TypeInt(bits, true);
+		auto uintType = new TypeInt(bits, false);
+
+		defaultTypes.emplace("int" + bits, intType);
+		defaultTypes.emplace("uint" + bits, uintType);
+
+		defaultIntTypes.push_back(intType);
+		defaultIntTypes.push_back(uintType);
+
+	}
+
+	std::vector<std::string> vecPrefixes = { "h", "", "d" };
+
+	//float types
+	size_t vecPrefixOff = 0;
+	for (int bits = MIN_FLOAT_BITS_SUPPORTED;
+		bits <= MAX_FLOAT_BITS_SUPPORTED; bits *= 2)
+	{
+		auto floatType = new TypeFloat(bits);
+		defaultTypes.emplace("float" + bits, floatType);
+		defaultFloatTypes.push_back(floatType);
+
+		if (vecPrefixOff >= vecPrefixes.size())
+		{
+			continue;
+		}
+
+		for (size_t e = MIN_VECTOR_LEN_SUPPORTED;
+			e <= MAX_VECTOR_LEN_SUPPORTED; ++e)
+		{
+			//makes hvec, vec, and dvec types
+			//can support qvec types if some weirdo wants 128-bit float support
+			defaultTypes.emplace(vecPrefixes[vecPrefixOff] + ("vec" + e), new TypeVector(e, floatType));
+			++vecPrefixOff;
+
+		}
+
+	}
+
+	//int vector types
+
+	CompiledType* si32 = getIntType(4);
+
+	for (size_t e = MIN_VECTOR_LEN_SUPPORTED;
+		e <= MAX_VECTOR_LEN_SUPPORTED; ++e)
+	{
+		defaultTypes.emplace("ivec" + e, new TypeVector(e, si32));
+
+	}
+
+	for (size_t e = MIN_VECTOR_LEN_SUPPORTED;
+		e <= MAX_VECTOR_LEN_SUPPORTED; ++e)
+	{
+		defaultVectorTypes.push_back((TypeVector*)defaultTypes["vec" + e]);
+
+	}
+
+	//void type
+	defaultVoidType = new TypeVoid();
+	defaultTypes.emplace("void", defaultVoidType);
+
+	//typical aliases
+	addPermanentAlias("byte", "int8");
+	//TODO replace with proper character types that ensures chars get printed as chars
+	addPermanentAlias("char", "uint8");
+	addPermanentAlias("char8", "uint8");
+	addPermanentAlias("char32", "uint32");
+	addPermanentAlias("short", "int16");
+	addPermanentAlias("int", "int32");
+	addPermanentAlias("long", "int64");
+	addPermanentAlias("float", "float32");
+	addPermanentAlias("double", "float64");
+
+}
+
+CaliburnAssembler::~CaliburnAssembler()
+{
+	for (auto type : defaultTypes)
+	{
+		delete type.second;
+
+	}
+
+}
+
 CompiledType* CaliburnAssembler::resolveType(ParsedType* type)
 {
 	auto it = defaultTypes.find(type->getBasicName());
@@ -27,8 +118,7 @@ CompiledType* CaliburnAssembler::resolveType(ParsedType* type)
 
 	if (resolved->hasA(TypeAttrib::GENERIC))
 	{
-		if (type->generics.size() < ((SpecializedType*)resolved)->minGenerics ||
-			type->generics.size() > ((SpecializedType*)resolved)->maxGenerics)
+		if (type->generics.size() > resolved->maxGenerics)
 		{
 			//TODO complain
 			return nullptr;
--- compiler/assembler.h
+++ compiler/assembler.h
@@ -15,79 +15,9 @@ namespace caliburn
 		TypeVoid* defaultVoidType = nullptr;
 
 	public:
-		CaliburnAssembler()
-		{
-			//int types
-			for (int bits = 8; bits <= 512; bits *= 2)
-			{
-				auto intType = new TypeInt(bits, true);
-				auto uintType = new TypeInt(bits, false);
+		CaliburnAssembler();
 
-				defaultTypes.emplace("int" + bits, intType);
-				defaultTypes.emplace("uint" + bits, uintType);
-
-				defaultIntTypes.push_back(intType);
-				defaultIntTypes.push_back(uintType);
-
-			}
-
-			//float types
-			for (int bits = 16; bits <= 64; bits *= 2)
-			{
-				auto floatType = new TypeFloat(bits);
-				defaultTypes.emplace("float" + bits, floatType);
-				defaultFloatTypes.push_back(floatType);
-
-			}
-
-			//vector types
-
-			CompiledType* fp16 = getFloatType(2);
-			CompiledType* fp32 = getFloatType(4);
-			CompiledType* fp64 = getFloatType(8);
-			CompiledType* si32 = getIntType(4);
-
-			for (int e = 2; e <= 4; ++e)
-			{
-				defaultTypes.emplace("hvec" + e, new TypeVector(e, fp16));
-				defaultTypes.emplace("vec" + e, new TypeVector(e, fp32));
-				defaultTypes.emplace("dvec" + e, new TypeVector(e, fp64));
-				defaultTypes.emplace("ivec" + e, new TypeVector(e, si32));
-
-			}
-
-			for (size_t i = 2; i <= 4; ++i)
-			{
-				defaultVectorTypes.push_back((TypeVector*)defaultTypes["vec" + i]);
-			}
-
-			//void type
-			defaultVoidType = new TypeVoid();
-			defaultTypes.emplace("void", defaultVoidType);
-
-			//typical aliases
-			addPermanentAlias("byte", "int8");
-			//TODO replace with proper character types that ensures chars get printed as chars
-			addPermanentAlias("char", "uint8");
-			addPermanentAlias("char8", "uint8");
-			addPermanentAlias("char32", "uint32");
-			addPermanentAlias("short", "int16");
-			addPermanentAlias("int", "int32");
-			addPermanentAlias("long", "int64");
-			addPermanentAlias("float", "float32");
-			addPermanentAlias("double", "float64");
-
-		}
-
-		virtual ~CaliburnAssembler()
-		{
-			for (auto type : defaultTypes)
-			{
-				delete type.second;
-
-			}
-
-		}
+		virtual ~CaliburnAssembler();
 
 		//Scope helpers
 
