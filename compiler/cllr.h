0a026652e1bdf4091b6e42d598000105a50ca315
Jeff Perando <realmsofcaelum@gmail.com>
2022-08-30 04:13:17 -0700

    Expanded on CLLR

--- compiler/cllr.h
+++ compiler/cllr.h
@@ -2,6 +2,10 @@
 #pragma once
 
 #include <stdint.h>
+#include <string>
+#include <vector>
+
+#include "langcore.h"
 
 /*
 Caliburn Low-Level Representation (CLLR [pronounced Caller]):
@@ -21,14 +25,76 @@ namespace caliburn
 	{
 		enum class Opcode : uint32_t
 		{
+			UNKNOWN,
+			
+			SHADER,
+			SHADER_STAGE,
+			INPUT,
+			OUTPUT,
+			DESCRIPTOR,
+			SHADER_END,
+
+			FUNCTION,
+			FUNCTION_ARG,
+			FUNCTION_END,
+
+			VARIABLE,
+
+			CALL,
+			CALL_ARG,
+
+			TYPE_STRUCT,
+			TYPE_FLOAT,
+			TYPE_INT,
+			TYPE_BOOL,
+			TYPE_PTR,
+			TYPE_ARRAY,
+			TYPE_VECTOR,
+			TYPE_MATRIX,
+
+			STRUCT_MEMBER,
+			STRUCT_END,
+
+			LABEL,
+			JUMP,
+			JUMP_COND,
+
+			ASSIGN,
+			ASSIGN_ARRAY,
+			BINOP,
+			COMPARE,
+
+			VALUE_ARGUMENT,
+			VALUE_ARRAY,
+			VALUE_CONVERT,
+			VALUE_DEREF,
+			VALUE_DESCRIPTOR,
+			VALUE_INIT,
+			VALUE_LITERAL,
+			VALUE_MEMBER,
+			VALUE_NULL,
+			VALUE_SUBARRAY,
+			VALUE_VARIABLE,
+			
+			RETURN,
 
 		};
 
 		struct Instruction
 		{
 			Opcode op;
-			uint32_t operands[3];
+			uint32_t operands[3] {0};
 		};
+
+		struct CLLRPipeline
+		{
+			std::string name;
+			std::vector<std::string> strs;
+			std::vector<Instruction> code;
+
+		};
+
+
 
 	}
 
